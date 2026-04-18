8e972ba906034b68b0f52247fa3af96571edfdb8
Nicoolay <Rozhnov.nv1992@gmail.com>
2026-04-18 20:37:14 +0700

    Добавьте файлы проекта.

---
+++ Source/LeaveMeAlone/Public/MyGameModeBase.h
@@ -0,0 +1,17 @@
+// LeaveMeAlone Game by Netologiya. All RightsReserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEAVEMEALONE_API AMyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
