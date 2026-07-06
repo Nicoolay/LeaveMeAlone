24dabe47d99dc731a30ceb17c3f21a1c6628d1cc
Nicoolay <Rozhnov.nv1992@gmail.com>
2026-07-06 11:06:55 +0700

    Add some AI logic

---
+++ Source/LeaveMeAlone/Private/Enemy/LMAEnemyCharacter.cpp
@@ -0,0 +1,14 @@
+// LeaveMeAlone Game by Netologiya. All RightsReserved.
+
+#include "Enemy/LMAEnemyCharacter.h"
+#include "Components/LMAHealthComponent.h"
+
+ALMAEnemyCharacter::ALMAEnemyCharacter() {
+  PrimaryActorTick.bCanEverTick = true;
+  HealthComponent =
+      CreateDefaultSubobject<ULMAHealthComponent>("HealthComponent");
+}
+
+void ALMAEnemyCharacter::BeginPlay() { Super::BeginPlay(); }
+
+void ALMAEnemyCharacter::Tick(float DeltaTime) { Super::Tick(DeltaTime); }
